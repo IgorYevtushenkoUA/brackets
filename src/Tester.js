8de4b7c431e59fe1e0b96ce363c3870b7fe1ce15
IgorYevtushenkoUA <igor.yevtushenko.ua@gmail.com>
2020-09-11 13:40:34 +0300

    feat: create basic index methods (18/20)

---
+++ src/Tester.js
@@ -0,0 +1,3 @@
+let  check = require('./index.js')
+console.log(check('|(|)', [['1', '2'], ['3', '4'], ['5', '6'], ['7', '7'], ['8', '8']])); // -> true
+
--- src/index.js
+++ src/index.js
@@ -1,3 +1,47 @@
+const isCloseBrackets = (bracket, bracketsConfig) => {
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        if (bracketsConfig[i][1] === bracket)
+            return true
+    }
+    return false
+}
+
+const isOpenBrackets = (bracket, bracketsConfig) => {
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        if (bracketsConfig[i][0] === bracket)
+            return true
+    }
+    return false
+}
+
+const isBracketsCouple = (bracketsOpen, bracketsClose, bracketsConfig) => {
+    if (!(isOpenBrackets(bracketsOpen, bracketsConfig) && isCloseBrackets(bracketsClose, bracketsConfig))) return false
+    for (let i = 0; i < bracketsConfig.length; i++)
+        if (bracketsConfig[i][0] === bracketsOpen && bracketsConfig[i][1] === bracketsClose)
+            return true
+    return false
+
+}
+
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let bracketsQueue = []
+
+    for (let i = 0; i < str.length; i++) {
+        if (bracketsQueue.length > 0 && str.charAt(i) === '|' && bracketsQueue[bracketsQueue.length - 1] === '|') {
+            bracketsQueue.pop()
+        } else if (str.charAt(i) === '{' || str.charAt(i) === '[' || str.charAt(i) === '(') {
+            bracketsQueue.push(str.charAt(i))
+        } else if (
+            (str.charAt(i) === ')' && bracketsQueue[bracketsQueue.length - 1] === '(')
+            ||
+            (str.charAt(i) === ']' && bracketsQueue[bracketsQueue.length - 1] === '[')
+            ||
+            (str.charAt(i) === '}' && bracketsQueue[bracketsQueue.length - 1] === '{')) {
+            bracketsQueue.pop()
+        } else {
+            bracketsQueue.push(str.charAt(i))
+        }
+    }
+    // console.log(bracketsQueue)
+    return bracketsQueue.length === 0
 }
